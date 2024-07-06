e6a8ab698bd5fc9ccd23c263f72af5eee264028b
Swetha-6281 <yarvaswetha9640@gmail.com>
2024-07-06 12:38:31 +0530

    first commit

---
+++ BasicProgram.java
@@ -0,0 +1,28 @@
+package JavaProgrammes;
+
+public class BasicProgram {
+	int a=20;
+	int b=30;
+	 
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		int c=a+b;
+
+		System.out.println("Hi  Hello "+"How are you");
+	
+		
+		
+			System.out.println("Hi How are you");
+		int i=1;
+		while(i<=5) {
+			i++;
+			
+			
+		}
+		
+
+	}
+
+}
---
+++ Ravindra.java
@@ -0,0 +1,10 @@
+package JavaProgrammes;
+
+public class Ravindra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
